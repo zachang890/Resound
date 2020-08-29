cbd4fb7c792ac648adbc99b71a09c63c6694e7b3
zachang890 <zachang890@gmail.com>
2020-08-28 22:21:48 -0700

    Main backend

---
+++ Backend/Resound-Backend/src/test/java/com/Backend/ResoundBackend/ResoundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.ResoundBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResoundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
