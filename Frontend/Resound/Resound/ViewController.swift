1797eff10ae23534f7893d7deec7b24a534fca4f
zachang890 <zachang890@gmail.com>
2020-08-30 14:56:53 -0700

    Frontend first commit

---
+++ Frontend/Resound/Resound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resound
+//
+//  Created by Zachary Chang on 8/30/20.
+//  Copyright © 2020 Zachary Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
