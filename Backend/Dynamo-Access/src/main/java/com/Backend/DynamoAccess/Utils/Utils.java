4245a1e4a0b5909c42ced9cd0ee71355e471185c
Zachary Chang <zachang890@gmail.com>
2020-09-09 16:25:12 -0700

    Update Utils.java

--- Backend/Dynamo-Access/src/main/java/com/Backend/DynamoAccess/Utils/Utils.java
+++ Backend/Dynamo-Access/src/main/java/com/Backend/DynamoAccess/Utils/Utils.java
@@ -4,8 +4,6 @@ public class Utils {
 
     public static final String SERVICE_ENDPOINT = "dynamodb.us-west-2.amazonaws.com";
     public static final String SIGNING_REGION = "us-west-2";
-    public static final String ACCESS_KEY = "AKIA2MJKXII6GOBQ3YXS"; //IAM user restricted
-    public static final String SECRET_KEY = "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";
 
     public static final String REMOVED = "Removed";
     public static final String UPDATED = "Updated";
