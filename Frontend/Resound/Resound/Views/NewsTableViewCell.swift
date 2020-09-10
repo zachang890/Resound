9c18900c02c2d97943b337a2f7a4656332936789
zachang890 <zachang890@gmail.com>
2020-09-10 11:43:41 -0700

    Table view start

---
+++ Frontend/Resound/Resound/Views/NewsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  NewsTableViewCell.swift
+//  Resound
+//
+//  Created by Zachary Chang on 9/10/20.
+//  Copyright © 2020 Zachary Chang. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet var headline: UILabel!
+    @IBOutlet var source: UILabel!
+    @IBOutlet var time: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
