2eb6e0622980ffdba4222369066a368158c8a5f3
zachang890 <zachang890@gmail.com>
2020-09-11 15:23:25 -0700

    Start EC2 configs

--- Frontend/Resound/Resound/Controllers/HeadlinesTableViewController.swift
+++ Frontend/Resound/Resound/Controllers/HeadlinesTableViewController.swift
@@ -49,7 +49,31 @@ class HeadlinesTableViewController: UITableViewController {
 
     
     
-    func retrieveTitles() {
-        
+    func accessDynamo(topic: String) {
+        let postEndpoint: String = "http://backend-main.eba-24mqhk9g.us-west-2.elasticbeanstalk.com/suggestions/today-topics-list"
+        let session = URLSession.shared
+        let url = URL(string: postEndpoint)!
+                
+        session.dataTask(with: url, completionHandler: { ( data: Data?, response: URLResponse?, error: Error?) -> Void in
+            guard let realResponse = response as? HTTPURLResponse, realResponse.statusCode == 200 else {
+                print("Not a 200 response")
+                return
+            }
+                
+            do {
+                if let ipString = NSString(data:data!, encoding: String.Encoding.utf8.rawValue) {
+                    print(ipString)
+                
+                    let jsonDictionary = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                    let origin = jsonDictionary["list"] as! [String]
+                   
+                    self.performSelector(onMainThread: #selector(TodayTopicController.updateFirstLabel(_:)), with: origin[0], waitUntilDone: false)
+                    self.performSelector(onMainThread: #selector(TodayTopicController.updateSecondLabel(_:)), with: origin[1], waitUntilDone: false)
+                    self.performSelector(onMainThread: #selector(TodayTopicController.updateThirdLabel(_:)), with: origin[2], waitUntilDone: false)
+                }
+            } catch {
+                print("bad things happened")
+            }
+        } ).resume()
     }
 }
